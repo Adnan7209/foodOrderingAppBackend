2cb9c4cf4374fc95f7ebb98937645f681417f9e8
Adnan7209 <mdadnan28713@gmail.com>
2024-03-05 13:30:46 +0530

    db connected successfull

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express,{Request,Response} from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
 const PORT = 7000;
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+.then(()=>console.log("db connection successful"));
+
 const app = express();
 app.use(express.json());
 app.use(cors());
