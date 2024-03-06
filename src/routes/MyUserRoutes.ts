3bc12ad5e86354628ccf4426e393a9d0b3228b70
Adnan7209 <mdadnan28713@gmail.com>
2024-03-06 15:26:30 +0530

    update-user-profile api created with authorization and validation

--- src/routes/MyUserRoutes.ts
+++ src/routes/MyUserRoutes.ts
@@ -1,12 +1,15 @@
 import express from "express";
-import { createCurrentUser } from "../controllers/MyUserControllers";
-import { jwtCheck } from "../middlewares/auth";
+import {
+  createCurrentUser,
+  updateCurrentUser,
+} from "../controllers/MyUserControllers";
+import { jwtCheck, jwtParse } from "../middlewares/auth";
+import { validateMyUserRequest } from "../middlewares/validation";
 
 const router = express.Router();
 
-
 //  api/my/user/
-router.post("/",jwtCheck,createCurrentUser);
-
+router.post("/", jwtCheck, createCurrentUser);
+router.put("/",jwtCheck,jwtParse,validateMyUserRequest, updateCurrentUser);
 
 export default router;
