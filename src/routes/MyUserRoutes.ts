726bf919f40ceff1c9bca436c96e87690d4afda7
Adnan7209 <mdadnan28713@gmail.com>
2024-03-06 14:19:23 +0530

    validated acces token with auth0

--- src/routes/MyUserRoutes.ts
+++ src/routes/MyUserRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { createCurrentUser } from "../controllers/MyUserControllers";
+import { jwtCheck } from "../middlewares/auth";
 
 const router = express.Router();
 
 
 //  api/my/user/
-router.post("/",createCurrentUser);
+router.post("/",jwtCheck,createCurrentUser);
 
 
 export default router;
